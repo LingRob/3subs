434962eea39beca67430e7d32cd38ecaf5a26c4a
LingRob <35962609+LingRob@users.noreply.github.com>
2022-06-16 12:58:10 +0100

    number 1

--- app/src/main/java/com/example/myapplicationreturninput/MainActivity.java
+++ app/src/main/java/com/example/myapplicationreturninput/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.myapplicationreturninput;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void handleText(View v){
+        EditText t = findViewById(R.id.source);
+        String input = t.getText().toString();
 
+        Log.d(tag:"info", input);
     }
 }
